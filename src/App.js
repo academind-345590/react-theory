870906c20550b754486249195aed03c4b0918cfc
excavator <str_token@mail.ru>
2019-11-07 12:55:54 +0300

    limit JSX: className, otherall wrapping

--- src/App.js
+++ src/App.js
@@ -3,23 +3,14 @@ import './App.css';
 
 class App extends Component {
   render(){
-    // return (
-    //   <div className="App">
-    //     <h1> Hello word !</h1>
-    //   </div>
-    // );
-
-    return React.createElement(
-      'div',
-      {
-        className: 'App'
-      },
-      React.createElement(
-        'h1',
-        null,
-        'Hello world !!'
-      )
-    );
+    return (
+      <div>
+        <div className="App">
+          <h1> Hello word !</h1>
+        </div>
+        <p>Something yet ...</p>
+      </div>
+    );    
   }
 }
 
