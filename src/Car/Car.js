650b6ca2cbe3b5cbc2d4143a90f1ee2377fc3faf
excavator <str_token@mail.ru>
2019-11-11 12:38:29 +0300

    Styles: dinamic classes

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './Car.css';
 
-export default props => (
-  <div className="Car">    
-    <h3>Car name: { props.name }</h3>
-    <p>Year: <strong>{ props.year }</strong></p>
-    <input type="text" onChange={props.onChaneName} value={props.name} />
-    <button onClick={props.onDelete}>Delete</button>
-  </div>
-)
+export default props => { 
+  const inputClasses = ['input'];
+
+  if(props.name !== ''){
+    inputClasses.push('green');
+  } else {
+    inputClasses.push('red');
+  }
+
+  if(props.name.length > 4){
+    inputClasses.push('bold');
+  }
+
+  return (
+    <div className="Car">    
+      <h3>Car name: { props.name }</h3>
+      <p>Year: <strong>{ props.year }</strong></p>
+      <input 
+        className={inputClasses.join(' ')}
+        type="text" 
+        onChange={props.onChaneName} 
+        value={props.name} 
+      />
+      <button onClick={props.onDelete}>Delete</button>
+    </div>
+  )
+}
