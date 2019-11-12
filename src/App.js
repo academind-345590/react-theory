faceb612f819e8f777099e4cca1a3844b5a7ef70
excavator <str_token@mail.ru>
2019-11-12 14:51:44 +0300

    Components: life cicle ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Car from './Car/Car';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
 
@@ -10,8 +11,8 @@ class App extends Component {
     this.state = {
       cars: [
         {name: 'Ford', year: 2005 },
-        // {name: 'Lada', year: 2015 },
-        // {name: 'Mazda', year: 1991 },
+        {name: 'Lada', year: 2015 },
+        {name: 'Mazda', year: 1991 },
       ],
       pageTitle: 'React components',
       showCars: false,
@@ -56,13 +57,14 @@ class App extends Component {
       if (this.state.showCars){
         cars = this.state.cars.map((car, index)=>{
           return (
-            <Car 
-              key = {index}
-              name = {car.name}
-              year = {car.year}
-              onDelete = {this.deleteHandler.bind(this, index)}
-              onChaneName = {(event) => this.onChaneName(event.target.value, index)}
-            />
+            <ErrorBoundary key = {index}>
+              <Car               
+                name = {car.name}
+                year = {car.year}
+                onDelete = {this.deleteHandler.bind(this, index)}
+                onChaneName = {(event) => this.onChaneName(event.target.value, index)}
+              />
+            </ErrorBoundary>
         )
       }) 
     }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -15,18 +15,18 @@ class Car extends React.Component {
     console.log('Car componentWillUpdate', nextProps, nextState);
   }
 
-  static getDerivedStateFromProps(nextProps, prevState){
-    console.log('Car getDerivedStateFromProps', nextProps, prevState);
-    return prevState;
-  }
+  // static getDerivedStateFromProps(nextProps, prevState){
+  //   console.log('Car getDerivedStateFromProps', nextProps, prevState);
+  //   return prevState;
+  // }
 
   componentDidUpdate(){
     console.log('Car componentDidUpdate');
   }
 
-  getSnapshotBeforeUpdate(){
-    console.log('Car getSnapshotBeforeUpdate');
-  }
+  // getSnapshotBeforeUpdate(){
+  //   console.log('Car getSnapshotBeforeUpdate');
+  // }
 
   // life cicle destroy
   componentWillUnmount(){
@@ -35,6 +35,11 @@ class Car extends React.Component {
 
   render(){
     console.log('Car render');
+
+    if(Math.random()>0.7){
+      throw new Error('Car random failed');
+    }
+    
     const inputClasses = [classes.input];
     const style = {
       border: '1px solid #ccc',
