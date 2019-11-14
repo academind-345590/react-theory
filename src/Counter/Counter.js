6a82f82e92b3d9804fb8fe43132bdf52f4699598
excavator <str_token@mail.ru>
2019-11-14 14:04:27 +0300

    Components: React.Fragment

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Auxiliary from '../hoc/Auxiliary'
 
 export default class Counter extends React.Component {
   state = {
@@ -12,18 +13,19 @@ export default class Counter extends React.Component {
   }
 
   render (){
-    // return (
-    //   <div>
-    //     <h2>Counter {this.state.counter}</h2>
-    //     <button onClick={this.addCounter}>+</button>
-    //     <button onClick={()=> this.setState({counter: this.state.counter -1})}>-</button>
-    //   </div>
-    // )
+    return (
+      // Auxiliary = React.Fragment or <></>
+      <Auxiliary>
+        <h2>Counter {this.state.counter}</h2>
+        <button onClick={this.addCounter}>+</button>
+        <button onClick={()=> this.setState({counter: this.state.counter -1})}>-</button>
+      </Auxiliary>
+    )
 
-    return [
-      <h2 key={'1'}>Counter {this.state.counter}</h2>,
-      <button key={'2'} onClick={this.addCounter}>+</button>,
-      <button key={'3'} onClick={()=> this.setState({counter: this.state.counter -1})}>-</button>,
-    ]
+    // return [
+    //   <h2 key={'1'}>Counter {this.state.counter}</h2>,
+    //   <button key={'2'} onClick={this.addCounter}>+</button>,
+    //   <button key={'3'} onClick={()=> this.setState({counter: this.state.counter -1})}>-</button>,
+    // ]
   }
 }
